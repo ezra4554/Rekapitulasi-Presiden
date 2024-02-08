6b8c4f2e05e311f2b396d6df98682bbee1f74d97
Hiyaa <ezra4554@gmail.com>
2024-02-08 21:38:06 +0700

    fix bulk create new users

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,24 +1,26 @@
-import express from 'express'
-import userController from '../controllers/userController.js'
+import express from "express";
+import userController from "../controllers/userController.js";
 import {
   protectUserTpsRoute,
   protectAdminRoute,
-} from '../middlewares/protectRoute.js'
+} from "../middlewares/protectRoute.js";
 
-const router = express.Router()
+const router = express.Router();
 
+// Create bulk new users
+router.post("/bulk", protectUserTpsRoute, userController.createBulkNewUsers);
 // Create a new user
-router.post('/', protectUserTpsRoute, userController.createNewUser)
+router.post("/", protectUserTpsRoute, userController.createNewUser);
 // Delete multiple users
-router.post('/deleteUsers', protectAdminRoute, userController.deleteUsers)
+router.post("/deleteUsers", protectAdminRoute, userController.deleteUsers);
 
 // Get all users
-router.get('/', protectAdminRoute, userController.getAllUsers)
+router.get("/", protectAdminRoute, userController.getAllUsers);
 
 // Update a user
-router.put('/:userId', protectAdminRoute, userController.updateUser)
+router.put("/:userId", protectAdminRoute, userController.updateUser);
 
 // Delete a user
-router.delete('/:userId', protectAdminRoute, userController.deleteUser)
+router.delete("/:userId", protectAdminRoute, userController.deleteUser);
 
-export default router
+export default router;
