9f649c77d4868bbc88a7bdae2ea96ead5a311343
Hiyaa <ezra4554@gmail.com>
2024-02-13 21:19:32 +0700

    add to production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -26,8 +26,44 @@ export default defineConfig({
       },
     ],
   },
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import checker from 'vite-plugin-checker';
+
+// ----------------------------------------------------------------------
+
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
+      },
+    }),
+  ],
+  resolve: {
+    alias: [
+      {
+        find: /^~(.+)/,
+        replacement: path.join(process.cwd(), 'node_modules/$1'),
+      },
+      {
+        find: /^src(.+)/,
+        replacement: path.join(process.cwd(), 'src/$1'),
+      },
+    ],
+  },
   server: {
     port: 3030,
+    // Get rid of the CORS error
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   preview: {
     port: 3030,
